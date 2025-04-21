29bb13fce6b88650d4616587dfcda6fba10b02ca
Jesse Hunt <jesse@Simba.local>
2025-04-21 12:23:56 +1000

    fix: gesture handler setup and new architecture config

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SessionProvider } from './src/context/SessionContext';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <SessionProvider>
-      <AppNavigator />
-    </SessionProvider>
+    <GestureHandlerRootView style={styles.container}>
+      <SessionProvider>
+        <AppNavigator />
+      </SessionProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
